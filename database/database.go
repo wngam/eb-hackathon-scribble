47dfd0f275bdc74fc26422abeb2198b2726fe6b2
Thomas Praeger <praegt@amazon.com>
2020-12-18 13:53:43 -0700

    remove unused import

--- database/database.go
+++ database/database.go
@@ -5,7 +5,6 @@ import (
 	"sort"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"log"
